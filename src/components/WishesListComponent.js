8874f9a8f7f42e69e3ddfbee3aab598c9ab9a60c
lindeluhuang <linde.lu.huang@gmail.com>
2020-10-26 13:20:20 -0400

    Fixed styling with app css

--- src/components/WishesListComponent.js
+++ src/components/WishesListComponent.js
@@ -9,12 +9,12 @@ import { Link } from 'react-router-dom';
 function RenderWishItem({ wish }) {
     return (
         <Card className="wishItem">
-            <Link to={`/wish/${wish.id}`}>
-                <CardBody>
+            <CardBody>
+                <Link to={`/wish/${wish.id}`}>
                     <CardTitle>{wish.content}</CardTitle>
-                    <CardSubtitle><img src="/assets/images/Heart.png" class="heart-icon" /> {wish.hearts} hearts • {wish.datedisplay}</CardSubtitle>
-                </CardBody>
-            </Link>
+                </Link>
+                <CardSubtitle><img src="/assets/images/Heart.png" class="heart-icon" /> {wish.hearts} hearts • {wish.datedisplay}</CardSubtitle>
+            </CardBody>
         </Card>
     );
 }
