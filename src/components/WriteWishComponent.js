3bc1fbc535ba39d3957f5ea6b928e385b0f656b4
lindeluhuang <linde.lu.huang@gmail.com>
2020-11-05 21:32:29 -0500

    Redux forms works

--- src/components/WriteWishComponent.js
+++ src/components/WriteWishComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import {
-    Button, Form, FormGroup, Input, Col, FormFeedback
-} from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem,
+    Button, Label, Col, Row } from 'reactstrap';
 import { Redirect } from 'react-router-dom';
+import { Control, LocalForm } from 'react-redux-form';
 
 class WriteWish extends Component {
     constructor(props) {
@@ -15,98 +15,64 @@ class WriteWish extends Component {
             }
         };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    validate(wish) {
 
-        const errors = {
-            wish: ''
-        };
-
-        if (this.state.touched.wish) {
-            if (wish.length < 10) {
-                errors.wish = 'Write just a bit more to fully realize your wish.';
-            }
-        }
-
-        return errors;
+    handleSubmit(values) {
+        console.log("Current state is: " + JSON.stringify(values));
+        alert("Current state is: " + JSON.stringify(values));
     }
+    // handleSubmit(event) {
+    // // alert('Current state is: ' + JSON.stringify(this.state));
+    // const newWishObj = {
+    //     id: Math.floor(Math.random() * Math.floor(99999)),
+    //     content: this.state.wish,
+    //     datedisplay: this.setDateDisplay(),
+    //     datetime: this.setDateTime(),
+    //     hearts: 0
+    // }
+    // event.preventDefault();
 
-    handleBlur = (field) => () => {
-        this.setState({
-            touched: { ...this.state.touched, [field]: true }
-        });
-    }
+    // if (!this.validate(this.state.wish).wish && this.state.wish.length) {
+    // // this.props.setNewWish(newWishObj);
+    // this.props.history.goBack()
+    // }
+    // }
 
-    handleInputChange(event) {
-        const target = event.target;
-        const name = target.name;
-        const value = target.value;
+    // setDateTime() {
+    //     var d = new Date();
+    //     return d;
+    // }
 
-        this.setState({
-            [name]: value
-        });
-    }
-
-    handleSubmit(event) {
-        // alert('Current state is: ' + JSON.stringify(this.state));
-        const newWishObj = {
-            id: Math.floor(Math.random() * Math.floor(99999)),
-            content: this.state.wish,
-            datedisplay: this.setDateDisplay(),
-            datetime: this.setDateTime(),
-            hearts: 0
-        }
-        event.preventDefault();
-
-        if (!this.validate(this.state.wish).wish && this.state.wish.length) {
-        // this.props.setNewWish(newWishObj);
-        this.props.history.goBack()
-    }
-
-    }
-
-    setDateTime() {
-        var d = new Date();
-        return d;
-    }
-
-    setDateDisplay() {
-        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        var d = new Date();
-        var month = months[d.getMonth()];
-        var year = d.getFullYear().toString().substr(-2);
-        return (month + ' `' + year)
-    }
+    // setDateDisplay() {
+    //     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    //     var d = new Date();
+    //     var month = months[d.getMonth()];
+    //     var year = d.getFullYear().toString().substr(-2);
+    //     return (month + ' `' + year)
+    // }
 
     render() {
-
-        const errors = this.validate(this.state.wish);
 
         return (
             <div className="row row-content">
                 <div className="col">
-                    <Form onSubmit={this.handleSubmit}>
-                        <FormGroup row>
+                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <Row className="form-group">
                             <Col>
-                                <Input type="textarea" id="wish" name="wish"
-                                    rows="12"
+                                <Control.textarea model=".wish" id="wish" name="wish"
+                                    className="form-control"
                                     placeholder="I wish..."
-                                    value={this.state.wish}
-                                    invalid={errors.wish}
-                                    onBlur={this.handleBlur("wish")}
-                                    onChange={this.handleInputChange} />
-                                <FormFeedback>{errors.wish}</FormFeedback>
+                                />
                             </Col>
-                        </FormGroup>
-                        <FormGroup row>
+                        </Row>
+                        <Row className="form-group">
                             <Col>
-                                <Button outline className="btn-release-wish">Release Your Wish</Button>
+                                <Button outline type="submit" className="btn-release-wish">Release Your Wish</Button>
                             </Col>
-                        </FormGroup>
-                    </Form>
+                        </Row>
+                    </LocalForm>
                 </div>
             </div>
         );
